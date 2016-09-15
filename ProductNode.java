becb6c654e2cee39446cf1c4261e512ceebbee6c
shanedonaldson01 <shanedonaldson01@gmail.com>
2016-09-15 17:57:24 -0500

    Create ProductNode.java

---
+++ ProductNode.java
@@ -0,0 +1,114 @@
+import java.util.Stack;
+
+class ProductNode
+   {
+   private Node root;             // first node of tree
+
+// -------------------------------------------------------------
+   public ProductNode()                  // constructor
+      { root = null; }            // no nodes in tree yet
+// -------------------------------------------------------------
+   public Node find(int key)      // find node with given key
+      {                           // (assumes non-empty tree)
+      Node current = root;               // start at root
+      while(current.prodIDOne != key)        // while no match,
+         {
+         if(key < current.prodIDOne)         // go left?
+            current = current.leftChild;
+         else                            // or go right?
+            current = current.rightChild;
+         if(current == null)             // if no child,
+            return null;                 // didn't find it
+         }
+      return current;                    // found it
+      }  // end find()
+// -------------------------------------------------------------
+   public void insertObjOne(int modelNumOne, String description, double cost, double price, int quantity)
+      {
+      Node newNode = new Node();    // make new node
+      newNode.prodIDOne = modelNumOne;           // insert data
+      newNode.description = description;
+      newNode.cost = cost;
+      newNode.price = price;
+      newNode.quantity = quantity;
+      
+      if(root==null)                // no node in root
+         root = newNode;
+      else                          // root occupied
+         {
+         Node current = root;       // start at root
+         Node parent;
+         while(true)                // (exits internally)
+            {
+            parent = current;
+            if(modelNumOne < current.prodIDOne)  // go left?
+               {
+               current = current.leftChild;
+               if(current == null)  // if end of the line,
+                  {                 // insert on left
+                  parent.leftChild = newNode;
+                  return;
+                  }
+               }  // end if go left
+            else                    // or go right?
+               {
+               current = current.rightChild;
+               if(current == null)  // if end of the line
+                  {                 // insert on right
+                  parent.rightChild = newNode;
+                  return;
+                  }
+               }  // end else go right
+            }  // end while
+         }  // end else not root
+      }  // end insert()
+// -------------------------------------------------------------
+   public void displayTree()
+      {
+      Stack globalStack = new Stack();
+      globalStack.push(root);
+      int nBlanks = 32;
+      boolean isRowEmpty = false;
+      System.out.println(
+      "...........................................................................");
+      while(isRowEmpty==false)
+         {
+         Stack localStack = new Stack();
+         isRowEmpty = true;
+
+         for(int j=0; j<nBlanks; j++)
+            System.out.print(' ');
+
+         while(globalStack.isEmpty()==false)
+            {
+            Node temp = (Node)globalStack.pop();
+            if(temp != null)
+               {
+               System.out.print(temp.prodIDOne);
+               localStack.push(temp.leftChild);
+               localStack.push(temp.rightChild);
+
+               if(temp.leftChild != null ||
+                                   temp.rightChild != null)
+                  isRowEmpty = false;
+               }
+            else
+               {
+               System.out.print("--");
+               localStack.push(null);
+               localStack.push(null);
+               }
+            for(int j=0; j<nBlanks*2-2; j++)
+               System.out.print(' ');
+            }  // end while globalStack not empty
+         System.out.println();
+         nBlanks /= 2;
+         while(localStack.isEmpty()==false)
+            globalStack.push( localStack.pop() );
+         }  // end while isRowEmpty is false
+      System.out.println(
+      "...........................................................................");
+      }  // end displayTree()
+// -------------------------------------------------------------
+   }  // end class Tree
+
